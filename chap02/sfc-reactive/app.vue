c2b1a61da729d9b77d95735a474b4fe7cdd0f07c
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-02-22 18:22:38 +0900

    sfc-reactiveの学習完了

--- chap02/sfc-reactive/app.vue
+++ chap02/sfc-reactive/app.vue
@@ -1,5 +1,32 @@
+<script setup lang="ts">
+import {onMounted, onUnmounted} from 'vue';
+
+const widthInit = Math.round(Math.random() * 10);
+const heightInit = Math.round(Math.random() * 10);
+const rectangle = reactive({
+  width: widthInit,
+  height: heightInit,
+});
+const area = computed(
+  ():number => {
+    return rectangle.width * rectangle.height;
+  }
+);
+
+onMounted(()=> {
+  const updateTimer = setInterval(
+    ():void => {
+      rectangle.width = Math.round(Math.random() * 10);
+      rectangle.height = Math.round(Math.random() * 10);
+    },
+    1000
+  );
+  onUnmounted(() => {
+    clearInterval(updateTimer);
+  })
+});
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<p>縦{{rectangle.height}}で横が{{rectangle.width}}の長方形の面積は{{area}}</p>
 </template>
