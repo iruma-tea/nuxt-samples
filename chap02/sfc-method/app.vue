7d690f657f393d8f065dd60bd5a0cffc90a612cf
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-02-22 19:17:45 +0900

    sfc-methodの学習完了

--- chap02/sfc-method/app.vue
+++ chap02/sfc-method/app.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+const msg = ref("まだ");
+const onButtonClick = (label: string, event: Event): void => {
+  const target = event.target as HTMLButtonElement;
+  const text = target.innerText;
+  msg.value = `${label}と${text}`;
+};
+</script>
+
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <p>{{ msg }}</p>
+  <button v-on:click="onButtonClick('Hello', $event)">こんにちは</button>
 </template>
