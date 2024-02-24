98ab8f733212c4c439b64d419a319e0f6183eeb4
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-02-24 16:45:05 +0900

    useFetch関数の実装

--- chap04/usefetch/pages/WeatherInfo/[id].vue
+++ chap04/usefetch/pages/WeatherInfo/[id].vue
@@ -13,35 +13,22 @@ const selectedCity = computed(
     }
 );
 
-//天気情報のテンプレート変数を用意。
-// const weatherDescription = ref("");
+const params: {
+	lang: string;
+	q: string;
+	appid: string;
+} = {
+	lang: "ja",
+	q: selectedCity.value.q,
+	//APIキーのクエリパラメータ。ここに各自の文字列を記述する!!
+	appid: "xxxxxx"
+}
 
-const asyncData = await useAsyncData(
-	'/WeatherInfog/${route.params.id}',
-	(): Promise<any> => {
-		const weatherInfoUrl = "https://api.openweathermap.org/data/2.5/weather";
-		const params:{
-			lang: string;
-			q: string;
-			appid: string;
-		} =
-		{
-			//言語設定のクエリパラメータ
-			lang: "ja",
-			//都市を表すクエリパラメータ。
-			q: selectedCity.value.q,
-			//APIキーのクエリパラメータ。ここに各自の文字列を記述する!!
-			appid: "xxxxxx",
-		}
-		//クエリパラメータを生成。
-		const queryParams = new URLSearchParams(params);
-		//実際にアクセスするURLを生成。
-		const urlFull = `${weatherInfoUrl}?${queryParams}`;
-		const response = $fetch(urlFull);
-		return response;
-	},
+const asyncData = await useFetch(
+	"https://api.openweathermap.org/data/2.5/weather",
 	{
-		// pick: ["wather"],
+		key: '/WeatherInfog/${route.params.id}',
+		query: params,
 		transform: (data: any): string => {
 			const weatherArray = data.weather;
 			const weather = weatherArray[0];
@@ -49,10 +36,7 @@ const asyncData = await useAsyncData(
 		}
 	}
 );
-// const data = asyncData.data;
-// const weatherArray = data.value.weather;
-// const weather = weatherArray[0];
-// weatherDescription.value = weather.description;
+
 const weatherDescription = asyncData.data;
 
 </script>
