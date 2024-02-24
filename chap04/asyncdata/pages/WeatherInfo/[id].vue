e9b67186b6e0c4de3c663d3962aebbbed556f9cc
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-02-24 15:37:14 +0900

    useAsyncDataの実装

--- chap04/asyncdata/pages/WeatherInfo/[id].vue
+++ chap04/asyncdata/pages/WeatherInfo/[id].vue
@@ -16,31 +16,33 @@ const selectedCity = computed(
 //天気情報のテンプレート変数を用意。
 const weatherDescription = ref("");
 
-//アクセス先URLの基本部分の変数を用意。
-const weatherInfoUrl = "https://api.openweathermap.org/data/2.5/weather";
-//クエリパラメータの元データとなるオブジェクトリテラルを用意。
-const params:{
-	lang: string;
-	q: string;
-	appid: string;
-} =
-{
-	//言語設定のクエリパラメータ
-	lang: "ja",
-	//都市を表すクエリパラメータ。
-	q: selectedCity.value.q,
-	//APIキーのクエリパラメータ。ここに各自の文字列を記述する!!
-	appid: "xxxxxx"
-}
-//クエリパラメータを生成。
-const queryParams = new URLSearchParams(params);
-//実際にアクセスするURLを生成。
-const urlFull = `${weatherInfoUrl}?${queryParams}`;
-//URLに非同期でアクセスしてデータを取得。
-const response = await $fetch(urlFull) as any;
-// console.log(response);
-//天気情報JSONから天気データを取得し、テンプレート変数に格納。
-const weatherArray = response.weather;
+const asyncData = await useAsyncData(
+	'/WeatherInfog/${route.params.id}',
+	(): Promise<any> => {
+		const weatherInfoUrl = "https://api.openweathermap.org/data/2.5/weather";
+		const params:{
+			lang: string;
+			q: string;
+			appid: string;
+		} =
+		{
+			//言語設定のクエリパラメータ
+			lang: "ja",
+			//都市を表すクエリパラメータ。
+			q: selectedCity.value.q,
+			//APIキーのクエリパラメータ。ここに各自の文字列を記述する!!
+			appid: "xxxxxx"
+		}
+		//クエリパラメータを生成。
+		const queryParams = new URLSearchParams(params);
+		//実際にアクセスするURLを生成。
+		const urlFull = `${weatherInfoUrl}?${queryParams}`;
+		const response = $fetch(urlFull);
+		return response;
+	}
+);
+const data = asyncData.data;
+const weatherArray = data.value.weather;
 const weather = weatherArray[0];
 weatherDescription.value = weather.description;
 
