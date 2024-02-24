963a3e9bf6ead8580c1fc75f1d3004317014d1d0
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-02-24 16:30:10 +0900

    usefetchの学習準備完了

---
+++ chap04/usefetch/pages/WeatherInfo/[id].vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import type { City } from "@/interfaces";
+
+// ルートオブジェクトを用意
+const route = useRoute();
+// 都市情報リストをステートから取得
+const cityList = useState<Map<number, City>>("cityList");
+// ルートパラメータをもとに都市データを取得
+const selectedCity = computed(
+    (): City => {
+        const idNo = Number(route.params.id);
+        return cityList.value.get(idNo) as City;
+    }
+);
+
+//天気情報のテンプレート変数を用意。
+// const weatherDescription = ref("");
+
+const asyncData = await useAsyncData(
+	'/WeatherInfog/${route.params.id}',
+	(): Promise<any> => {
+		const weatherInfoUrl = "https://api.openweathermap.org/data/2.5/weather";
+		const params:{
+			lang: string;
+			q: string;
+			appid: string;
+		} =
+		{
+			//言語設定のクエリパラメータ
+			lang: "ja",
+			//都市を表すクエリパラメータ。
+			q: selectedCity.value.q,
+			//APIキーのクエリパラメータ。ここに各自の文字列を記述する!!
+			appid: "xxxxxx",
+		}
+		//クエリパラメータを生成。
+		const queryParams = new URLSearchParams(params);
+		//実際にアクセスするURLを生成。
+		const urlFull = `${weatherInfoUrl}?${queryParams}`;
+		const response = $fetch(urlFull);
+		return response;
+	},
+	{
+		// pick: ["wather"],
+		transform: (data: any): string => {
+			const weatherArray = data.weather;
+			const weather = weatherArray[0];
+			return weather.description;
+		}
+	}
+);
+// const data = asyncData.data;
+// const weatherArray = data.value.weather;
+// const weather = weatherArray[0];
+// weatherDescription.value = weather.description;
+const weatherDescription = asyncData.data;
+
+</script>
+
+<template>
+	<section>
+		<h2>{{selectedCity.name}}の天気</h2>
+		<p>{{weatherDescription}}</p>
+	</section>
+	<p>リストに<NuxtLink v-bind:to="{name: 'index'}">戻る</NuxtLink></p>
+</template>
