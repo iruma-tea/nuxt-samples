9b199b1c24ee6d04987db775d73759e01089eec4
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-02-25 15:52:44 +0900

    chap05の学習開始
    server-basicの学習準備完了

---
+++ chap05/server-basic/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>TOP</h1>
+    <nav id="breadcrumnbs">
+        <ul>
+            <li>TOP</li>
+        </ul>
+    </nav>
+    <section>
+        <p>
+            <NuxtLink v-bind:to="{name: 'member-memberList'}">会員管理はこちら</NuxtLink>
+        </p>
+    </section>
+</template>
---
+++ chap05/server-basic/pages/member/memberList.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { Member } from "@/interfaces";
+definePageMeta(
+    {
+        layout: "member"
+    }
+);
+
+// 会員情報リストをステートから取得
+const memberList = useState<Map<number, Member>>("memberList");
+</script>
+
+<template>
+    <nav id="breadcrumbs">
+        <ul>
+            <li><NuxtLink v-bind:to="{name: 'index'}">TOP</NuxtLink></li>
+            <li>会員情報</li>
+        </ul>
+    </nav>
+    <section>
+        <h2>会員リスト</h2>
+        <p>
+            新規登録は<NuxtLink v-bind:to="{name: 'member-memberAdd'}">こちら</NuxtLink>から
+        </p>
+        <section>
+            <ul>
+                <li v-for="[id, member] in memberList" v-bind:key="id">
+					<NuxtLink v-bind:to="{name: 'member-memberDetail-id', params: {id: id}}">
+						IDが{{id}}の{{member.name}}さん
+					</NuxtLink>
+                </li>
+            </ul>
+        </section>
+    </section>
+</template>
