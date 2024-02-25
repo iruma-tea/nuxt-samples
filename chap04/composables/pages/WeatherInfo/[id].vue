eac857d22bcd7428016f669a45af383589b105d4
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-02-25 14:58:51 +0900

    composablesの学習終了

--- chap04/composables/pages/WeatherInfo/[id].vue
+++ chap04/composables/pages/WeatherInfo/[id].vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { City } from "@/interfaces";
+import { useWeatherInfoFetcher } from "~/composables/useWeatherInfoFetcher";
 
 // ルートオブジェクトを用意
 const route = useRoute();
@@ -13,39 +14,8 @@ const selectedCity = computed(
     }
 );
 
-const asyncData = useLazyAsyncData(
-	'/WeatherInfog/${route.params.id}',
-	(): Promise<any> => {
-		const weatherInfoUrl = "https://api.openweathermap.org/data/2.5/weather";
-		const params:{
-			lang: string;
-			q: string;
-			appid: string;
-		} =
-		{
-			//言語設定のクエリパラメータ
-			lang: "ja",
-			//都市を表すクエリパラメータ。
-			q: selectedCity.value.q,
-			//APIキーのクエリパラメータ。ここに各自の文字列を記述する!!
-			appid: "xxxxxx",
-		}
-		//クエリパラメータを生成。
-		const queryParams = new URLSearchParams(params);
-		//実際にアクセスするURLを生成。
-		const urlFull = `${weatherInfoUrl}?${queryParams}`;
-		const response = $fetch(urlFull);
-		return response;
-	},
-	{
-		// pick: ["wather"],
-		transform: (data: any): string => {
-			const weatherArray = data.weather;
-			const weather = weatherArray[0];
-			return weather.description;
-		}
-	}
-);
+// コンポーザブル(useWeatherInofFetcher)の使用
+const asyncData = useWeatherInfoFetcher(selectedCity.value);
 const weatherDescription = asyncData.data;
 const pending = asyncData.pending;
 
