e6c10da3bf2736ef33eb1d18fa19d2eaaf6aaafa
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-05-06 16:26:45 +0900

    6.3. カスタムエラー画面の学習完了

---
+++ chap06/error-basic/components/ErrorGeneratorImmediate.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+throw createError("擬似エラー発生。");
+
+</script>
+
+<template>
+    <p>エラーを自動生成</p>
+</template>
--- chap06/error-basic/pages/index.vue
+++ chap06/error-basic/pages/index.vue
@@ -5,5 +5,15 @@
                 エラー表示実験
             </NuxtLink>
         </li>
+        <li>
+            <NuxtLink v-bind:to="{name: 'errorHandlerNavigate'}">
+                画面表示時のエラー実験
+            </NuxtLink>
+        </li>
+        <li>
+            <NuxtLink v-bind:to="{name: 'errorHandlerFatal'}">
+                致命的エラー発生実験
+            </NuxtLink>
+        </li>
     </ul>
 </template>
